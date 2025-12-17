6e0a158ae091e14f811514e3b1b7ccb68bf45e29
ecemergul <101472525+ecemergul@users.noreply.github.com>
2025-12-17 17:33:21 +0300

    post verisine göre hoşgeldin mesajı gösterme

--- lambdas/welcome_function/welcome_function.py
+++ lambdas/welcome_function/welcome_function.py
@@ -1,7 +1,11 @@
+import json
+
 def lambda_handler(event, context):
+    data = json.loads(event.get("body", "{}"))
+    name = data.get("name", "ziyaretçi")
+
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "text/html"},
-        "body": "<h1>Merhaba, teleskop projemize hos geldiniz!</h1>",
-        "isBase64Encoded": False
+        "body": f'<h1>Merhaba, "{name}"!</h1>'
     }
